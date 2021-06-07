2c8dbe90973fceb8a5c1a6db6d2ab7dda330b833
FORTECH\sorana.ciobanita <sorana.ciobanita@fortech.ro>
2021-06-07 23:20:35 +0300

    Display removed elements

--- public/app/content.js
+++ public/app/content.js
@@ -3,11 +3,27 @@
 const filterest = {
   elementHovered: false,
   elementHighlighted: false,
-  ancestorsNr: 0, // how far to travel up the line of ancestors
   isActive: false,
+  hiddenElements: [],
+  helpWindow: false,
+
+  isChildOfHelpWindow: function (e) {
+    while (e) {
+      if (e === filterest.helpWindow) return true;
+      return filterest.isChildOfHelpWindow(e.parentNode);
+    }
+
+    return false;
+  },
+
+  ignoreElement: function (e) {
+    return e.tagName === "BODY"
+      || e.tagName === "HTML"
+      || this.isChildOfHelpWindow(e);
+  },
 
   addHighlight: function () {
-    filterest.elementHighlighted.style.outline = "solid 2px red";
+    filterest.elementHighlighted.style.outline = "solid 2px #de8a8a";
     filterest.elementHighlighted.style.outlineOffset = "-2px";
   },
 
@@ -23,38 +39,26 @@ const filterest = {
       filterest.removeHighlight();
     }
 
+    if (filterest.ignoreElement(filterest.elementHovered)) return;
+
     filterest.elementHighlighted = filterest.elementHovered;
-
-    if (filterest.elementHighlighted.className === "filterest_overlay") {
-      filterest.elementHighlighted =
-        filterest.elementHighlighted.relatedElement;
-    }
-
-    let i = 0;
-    for (i = 0; i < filterest.ancestorsNr; i++) {
-      if (filterest.elementHighlighted.parentNode !== window.document) {
-        filterest.elementHighlighted = filterest.elementHighlighted.parentNode;
-      } else {
-        break;
-      }
-    }
-
-    filterest.ancestorsNr = i;
     filterest.addHighlight();
   },
 
   mouseover: function (e) {
     if (filterest.elementHovered !== e.target) {
-      filterest.ancestorsNr = 0;
       filterest.elementHovered = e.target;
       filterest.highlightElement();
     }
   },
 
-  hideTarget: function (e) {
-    console.log(filterest.elementHighlighted);
+  hideElement: function (e) {
+    if (filterest.ignoreElement(filterest.elementHovered)) return;
 
-    filterest.elementHighlighted.style.display = "none";
+    filterest.elementHighlighted.classList.add('filterest-hidden');
+    filterest.hiddenElements.push(filterest.elementHighlighted);
+
+    filterest.updateElementsList();
   },
 
   preventDefaultEvent: function (e) {
@@ -63,11 +67,66 @@ const filterest = {
     return false;
   },
 
+  getElementDisplayForTable: function (e) {
+    if (e.innerText) {
+      if (e.innerText.length > 100)
+        return e.innerText.substring(0, 100) + '...'
+      return e.innerText;
+    }
+    return e;
+  },
+
+  updateElementsList: function () {
+    if (!filterest.helpWindow) return;
+
+    let elemList = document.querySelector("#elements_list");
+    let elementsHtml = [];
+
+    if (filterest.hiddenElements.length) {
+      elementsHtml.push(`<table><tr><th>Removed element</th><th>Remember</th><th></th></tr>`);
+
+      filterest.hiddenElements.forEach(element => {
+        elementsHtml.push(`<tr>
+          <td>${filterest.getElementDisplayForTable(element)}</td>
+          <td><input type="checkbox"></td>
+          <td><button>Restore</button></td>
+        </tr>`)
+      });
+
+      elementsHtml.push('</table>');
+    }
+
+    elemList.innerHTML = elementsHtml.join('\n');
+  },
+
   activate: function () {
     filterest.isActive = true;
 
+    let div = document.createElement('div');
+    div.setAttribute('id', 'filterest_help_window');
+
+    div.innerHTML = `
+    <div id='filterest_help_window'>
+      <h3>Filterest</h3>
+      <button id="minimize">_</button>
+      <div id="elements_list"></div>
+    </div>
+    `;
+
+    div.querySelector('#minimize').addEventListener('click', function (e) {
+      console.log("minimize clicked");
+      div.classList.toggle('minimized');
+      e.preventDefault();
+    });
+
+    document.body.appendChild(div);
+
+    filterest.helpWindow = div;
+
+    filterest.updateElementsList();
+
     document.addEventListener("mouseover", filterest.mouseover, true);
-    document.addEventListener('mousedown', filterest.hideTarget, true);
+    document.addEventListener('mousedown', filterest.hideElement, true);
     document.addEventListener('mouseup', filterest.preventDefaultEvent, true);
     document.addEventListener('click', filterest.preventDefaultEvent, true);
 
@@ -78,7 +137,7 @@ const filterest = {
     filterest.isActive = false;
 
     document.removeEventListener("mouseover", filterest.mouseover, true);
-    document.removeEventListener('mousedown', filterest.hideTarget, true);
+    document.removeEventListener('mousedown', filterest.hideElement, true);
     document.removeEventListener('mouseup', filterest.preventDefaultEvent, true);
     document.removeEventListener('click', filterest.preventDefaultEvent, true);
 
@@ -87,6 +146,7 @@ const filterest = {
     }
 
     filterest.elementHighlighted = false;
+    document.body.removeChild(filterest.helpWindow);
 
     chrome.extension.sendMessage({ action: "status", active: false });
   },
